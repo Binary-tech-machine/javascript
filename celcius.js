a8b8f4ee688cef24f0ec6a5cb5ac65a00a1c0034
Harsh Agarwal <54469226+Binary-tech-machine@users.noreply.github.com>
2020-05-17 18:42:03 +0530

    Initial commit

---
+++ celcius.js
@@ -0,0 +1,12 @@
+var score =100
+score =130
+const bonus =50
+var finalScore =(score + bonus)*1.8 
+var complex = ((4 + 4)* 5)/2
+// console.log(finalScore)
+// console.log(complex)
+let tempInFahernheit = 100;
+// do calculation 
+
+let celsius = (tempInFahernheit-32)* 5/9;
+console.log(celsius)
---
+++ variable.js
@@ -0,0 +1,9 @@
+let name = 'john'
+let score =100.0
+let bonus =20
+
+let totalScore = score + bonus
+console.log(totalScore)
+let firstName = 'john'
+let lastName = 'doe'
+console.log(firstName+"-"+ lastName)
